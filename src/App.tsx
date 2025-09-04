8e2b7f83011c5448a03f491dd02bee718bd2cb6a
Seamus Finlayson <77430559+SeamusFinlayson@users.noreply.github.com>
2025-09-03 23:25:47 -0700

    Add button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { Settings2 } from "lucide-react";
+import { Button } from "./components/ui/button";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="">
+      <div className="bg-mirage-50 dark:bg-mirage-950 grid h-screen gap-2 p-4">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <Button>Accent</Button>
+        <Button variant={"secondary"}>Secondary</Button>
+        <Button variant={"ghost"}>Ghost</Button>
+        <Button size={"icon"} variant={"ghost"}>
+          <Settings2 className="shrink-0" />
+        </Button>
+        <Button variant={"outline"}>Outline</Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
