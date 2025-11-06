a5bd47dfce5bac9971b38ca465a0e0bd16660fbb
Seamus Finlayson <77430559+SeamusFinlayson@users.noreply.github.com>
2025-11-06 03:24:08 -0800

    Add leaf decoration to end of statblocks

--- src/statblockSearch/components/MonsterView.tsx
+++ src/statblockSearch/components/MonsterView.tsx
@@ -1,3 +1,4 @@
+import { LeafIcon } from "lucide-react";
 import type { MonsterDataBundle } from "../../types/monsterDataBundlesZod";
 import { FeatureBlock } from "../creatureBlockUI/FeatureBlock";
 import { StatBlock } from "../creatureBlockUI/StatBlock";
@@ -13,7 +14,13 @@ export default function MonsterView({
   return (
     <div className="bg-mirage-50 grid justify-items-center gap-y-8 p-4 text-sm text-black sm:p-6">
       <StatBlock statblock={monsterData.statblock} />
-      {/* <LeafIcon className="shrink-0" /> */}
+
+      <div className="flex w-full justify-center gap-2.5">
+        <LeafIcon className="size-4 shrink-0" />
+        <LeafIcon className="size-4 shrink-0" />
+        <LeafIcon className="size-4 shrink-0" />
+      </div>
+
       <div className="grid h-fit w-full gap-8">
         {monsterData.featuresBlocks.length > 0 &&
           monsterData.featuresBlocks.map((item) => (
