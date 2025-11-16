3e1b22fb07c8213a51dc7b49905bcfc437a058f0
Seamus Finlayson <77430559+SeamusFinlayson@users.noreply.github.com>
2025-11-15 20:58:44 -0800

    Remove LeafIcon from MonsterView

--- src/statblockViewer/MonsterView.tsx
+++ src/statblockViewer/MonsterView.tsx
@@ -1,4 +1,3 @@
-import { LeafIcon } from "lucide-react";
 import type { MonsterDataBundle } from "../types/monsterDataBundlesZod";
 import { FeatureBlock } from "./creatureBlockUI/FeatureBlock";
 import { StatBlock } from "./creatureBlockUI/StatBlock";
@@ -17,12 +16,6 @@ export default function MonsterView({
       <ScrollArea className="grow basis-0">
         <div className="bg-mirage-50 grid justify-items-center gap-y-8 p-4 text-sm text-black">
           <StatBlock statblock={monsterData.statblock} />
-
-          <div className="flex w-full justify-center gap-2.5">
-            <LeafIcon className="size-4 shrink-0" />
-            <LeafIcon className="size-4 shrink-0" />
-            <LeafIcon className="size-4 shrink-0" />
-          </div>
 
           <div className="grid h-fit w-full justify-items-center gap-8">
             {monsterData.featuresBlocks.length > 0 &&
