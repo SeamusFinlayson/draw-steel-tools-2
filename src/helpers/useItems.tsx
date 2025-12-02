e19e51c95228050db6c32edaf48021fc5bf1089e
Seamus Finlayson <77430559+SeamusFinlayson@users.noreply.github.com>
2025-12-02 01:04:35 -0800

    Remove debug logging from handleItems function in useItems hook

--- src/helpers/useItems.tsx
+++ src/helpers/useItems.tsx
@@ -7,7 +7,6 @@ export function useItems() {
   useEffect(() => {
     const handleItems = (items: Item[]) => {
       setItems(items);
-      console.log("handleItems");
     };
 
     OBR.scene.items.getItems().then(
