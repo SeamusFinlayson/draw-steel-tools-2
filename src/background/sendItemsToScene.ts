ecfb8eeecf1753c33d9a62e0b9ee602e53127942
Seamus Finlayson <77430559+SeamusFinlayson@users.noreply.github.com>
2025-12-02 16:25:05 -0800

    Make item sending function synchronous

--- src/background/sendItemsToScene.ts
+++ src/background/sendItemsToScene.ts
@@ -1,15 +1,15 @@
 import OBR, { type Item } from "@owlbear-rodeo/sdk";
 
-export async function sendItemsToScene(
+export function sendItemsToScene(
   addItemsArray: Item[],
   deleteItemsArray: string[],
 ) {
   // console.log("added items length", addItemsArray.length);
   // console.log("deleted items length", deleteItemsArray.length);
-  await Promise.all([
-    OBR.scene.local.deleteItems(deleteItemsArray),
-    OBR.scene.local.addItems(addItemsArray),
-  ]);
+
+  OBR.scene.local.deleteItems([...deleteItemsArray]);
+  OBR.scene.local.addItems([...addItemsArray]);
+
   deleteItemsArray.length = 0;
   addItemsArray.length = 0;
 }
